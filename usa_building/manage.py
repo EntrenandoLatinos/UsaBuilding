bfbe3a16fbc27bea8f8dfe54943eb362e07c08dc
CBM <camilobmoya21@gmail.com>
2024-03-08 18:10:32 -0500

    Project creation

--- usa_building/manage.py
+++ usa_building/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usa_building.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usa_building.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
